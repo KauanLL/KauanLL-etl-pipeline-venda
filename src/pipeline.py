2c5abccad52ce0b503fba7db593df91f32f97131
KauanLL <kauanlemos@outlook.com.br>
2025-09-29 17:52:40 -0300

    last commit

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,25 +1,35 @@
-from prefect import flow, task
+from prefect import flow, task, get_run_logger
+import pandas as pd
 from extract import extract_data
 from transform import transform_data
 from load import load_data
 
 @task
-def extract():
+def extract(path: str):
+    logger = get_run_logger()
+    logger.info(f'Lendo arquivo CSV de {path}')
     return extract_data()
 
 @task
-def transform(df):
+def transform(df: pd.DataFrame):
+    logger = get_run_logger()
+    logger.info(f'Iniciando transformação e validação dos dados')
     return transform_data(df)
 
 @task
-def load(df):
+def load(df: pd.DataFrame, db_path: str):
+    logger = get_run_logger()
+    logger.info(f'Carregando dados em {db_path}')
     load_data(df)
 
 @flow
-def etl_pipeline():
-    df = extract()
+def etl_pipeline(path: str, db_path: str):
+    logger = get_run_logger()
+    logger.info('Pipeline ETL inicializando')
+    df = extract(path)
     df = transform(df)
-    load(df)
+    load(df, db_path)
+    logger.info('Pipeline finalizado com sucesso')
     
 if __name__ == "__main__":
-    etl_pipeline()
+    etl_pipeline("data/vendas.csv", "data/vendas.db")
--- src/transform.py
+++ src/transform.py
@@ -1,8 +1,18 @@
 import pandas as pd
 
 
-def transform_data(df):
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     df.columns = [c.lower() for c in df.columns]
-    df['data'] = pd.to_datetime(df['data'])
-    df['valor'] = df['valor'].astype(float)
+    df['data'] = pd.to_datetime(df['data'], errors="coerce")
+    df['valor'] = pd.to_numeric(df['valor'], errors="coerce")
+
+    if df['valor'].isnull().any():
+        raise ValueError('Valores inválidos encontrado na coluna VALOR')
+
+    if (df['valor'] <= 0).any():
+        raise ValueError('Encontrado Valores menores ou igual a 0')
+
+    if df['data'].isnull().any():
+        raise ValueError('Valores inválidos encontrados na coluna DATA')
+
     return df
